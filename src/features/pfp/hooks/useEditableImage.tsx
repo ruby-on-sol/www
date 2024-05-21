56c90f80aa185ce39c9b3f315238d05cc39a1e3e
flick <github@0xflick.xyz>
2024-05-20 22:09:29 -0600

    memes

---
+++ src/features/pfp/hooks/useEditableImage.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { EditableImage } from "../fabric/EditableImage";
+import { PfpCircle } from "../fabric/PfpCircle";
+import { CanvasWithSafeArea } from "../helpers/CanvasWithSafeArea";
+import { type Point, util } from "fabric";
+
+export function useEditableImage({
+  canvas,
+  src,
+  onLoaded,
+}: {
+  canvas?: CanvasWithSafeArea | null;
+  src?: string;
+  onLoaded?: () => void;
+}) {
+  useEffect(() => {
+    if (!canvas) {
+      return;
+    }
+
+    if (!src) {
+      return;
+    }
+    const image = new Image();
+    image.src = src;
+    const editableImage = new EditableImage(image, {
+      // width: 400,
+      // height: 400,
+    });
+    image.onload = () => {
+      canvas.add(editableImage);
+      // canvas.add(
+      //   new PfpCircle({
+      //     editableImage,
+      //   })
+      // );
+      editableImage.setCoords();
+      editableImage.setupDragMatrix();
+      const ratio = 1;
+      editableImage.setFixedAspectData(ratio);
+      editableImage.resetToAspectRatio(ratio);
+
+      canvas.renderAll();
+      onLoaded?.();
+    };
+    return () => {
+      canvas.remove(editableImage);
+    };
+    // Don't re-add the image if the onLoad callback changes
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [canvas, src]);
+}
---
+++ src/features/pfp/stickers.ts
@@ -0,0 +1,22 @@
+export const stickers = [
+  {
+    src: "/stickers/bunny-ears.png",
+    label: "bunny ears",
+  },
+  {
+    src: "/stickers/blue-ears.png",
+    label: "bunny ears",
+  },
+  {
+    src: "/stickers/gold-ears.png",
+    label: "bunny ears",
+  },
+  {
+    src: "/stickers/green-ears.png",
+    label: "bunny ears",
+  },
+  {
+    src: "/stickers/tophat.png",
+    label: "top hat",
+  },
+];
